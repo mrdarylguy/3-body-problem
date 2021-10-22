7f04cad9fcbfd8f27e08806a095656eb78f64702
Daryl Tng <42925677+mrdarylguy@users.noreply.github.com>
2021-10-23 00:13:23 +0800

    create function for extraction of ring data

--- ring world/ring_world.py
+++ ring world/ring_world.py
@@ -1,3 +1,9 @@
+"""
+This is a script to extract astrophysical data and recommend 
+if the particular Halo is habitable.
+
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -30,21 +36,21 @@ class Halo:
         period = np.sqrt(self.gravity / radius)
         return period
 
-
-for ring in rings_to_study:
-    if ring in ring_data:
-        halo = Halo(number=ring["number"], 
-                    diameter=ring["diameter"],
-                    width=ring["width"],
-                    orbital_period=ring["orbital_period"],
-                    gravity=ring["gravity"],
-                    min_temp=ring["ring_temp"],
-                    max_temp=ring["max_temp"],
-                    attached_AI=ring["attached_AI"])
+    def extractRingData(self, rings_to_study):
+        for ring in rings_to_study:
+            if ring in ring_data:
+                halo = Halo(number=ring["number"], 
+                            diameter=ring["diameter"],
+                            width=ring["width"],
+                            orbital_period=ring["orbital_period"],
+                            gravity=ring["gravity"],
+                            min_temp=ring["ring_temp"],
+                            max_temp=ring["max_temp"],
+                            attached_AI=ring["attached_AI"])
 
         #Calcultate the orbital period if it is not given
-        if halo.orbital_period == None:
-            halo.orbital_period == Halo.calculateOrbitalPeriod
+                if halo.orbital_period == None:
+                    halo.orbital_period == self.calculateOrbitalPeriod
 
 
 rings_to_study = [] 
