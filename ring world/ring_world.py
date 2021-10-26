17ded4a9161c4b949299eb714495d3c43fa99557
Daryl Tng <42925677+mrdarylguy@users.noreply.github.com>
2021-10-27 00:29:25 +0800

    json load function outside the class

--- ring world/ring_world.py
+++ ring world/ring_world.py
@@ -37,11 +37,7 @@ class Halo:
         period = np.sqrt(self.gravity * 9.81 / radius)
         return period
 
-    def extractRingData(self, rings_to_study=type(list), ring_info=type(list)):
-
-        ring_data = open("ring_data.json")
-        ring_data = json.load(ring_data)
-
+    def extractRingData(self, rings_to_study, ring_info):
 
         for ring in rings_to_study:
             if ring in ring_data:
@@ -58,7 +54,7 @@ class Halo:
                 if halo.orbital_period == None:
                     halo.orbital_period == self.calculateOrbitalPeriod
 
-            ring_info.append(halo)
+                ring_info.append(halo)
 
     def habitability(self, ring_info):
         for halo in ring_info:
@@ -71,14 +67,9 @@ class Halo:
 
         pass
 
-if __name__ == "__main__":
+ring_data = open("ring_data.json")
+ring_data = json.load(ring_data) 
 
-    rings_to_study = ["04"]
-    ring_info = []
- 
-    _Halo = Halo()
-    _Halo.extractRingData(rings_to_study,
-                          ring_info)
-
-    print(ring_info)
+rings_to_study = ["04"]
+ring_info = []
 
